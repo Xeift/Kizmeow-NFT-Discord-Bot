f2243d95b9b630fdbf9f600b73dc3aca5ad1b81e
DOFER998 <67563470+DOFER998@users.noreply.github.com>
2022-06-29 17:35:51 +0300

    Adding a new command

--- Kizmeow NFT Tracker V3/main.py
+++ Kizmeow NFT Tracker V3/main.py
@@ -1,27 +1,30 @@
 import discord
 
-bot = discord.Bot(intents=discord.Intents.all(),)
+bot = discord.Bot(intents=discord.Intents.all(), )
+
+
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-extensions = [# load cogs
-  #--------------------system commands
-  'cogs.meow',
-  'cogs.invite',
-  'cogs.help',
-  #--------------------system commands
 
-  #--------------------NFT commands
-  'cogs.project_realtime',
-  'cogs.project_history',
-  'cogs.project_nft',
-  'cogs.contract_source_code'
-  #--------------------NFT commands
+extensions = [  # load cogs
+    # --------------------system commands
+    'cogs.meow',
+    'cogs.invite',
+    'cogs.help',
+    # --------------------system commands
+
+    # --------------------NFT commands
+    'cogs.project_rarity',
+    'cogs.project_realtime',
+    'cogs.project_history',
+    'cogs.project_nft'
+    # --------------------NFT commands
 ]
 
-if __name__ == '__main__':# import cogs from cogs folder
-	for extension in extensions:
-		bot.load_extension(extension) 
-        
-bot.run('YOUR_TOKEN_FROM_DEV_PORTAL')# bot token
+if __name__ == '__main__':  # import cogs from cogs folder
+    for extension in extensions:
+        bot.load_extension(extension)
+
+bot.run('BOT_TOKEN')  # bot token
