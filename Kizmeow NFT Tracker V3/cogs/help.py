12dbd53924b97b88a2bc35abd4ebb634641f798f
DOFER998 <67563470+DOFER998@users.noreply.github.com>
2022-06-29 17:32:19 +0300

    Little tweaks

--- Kizmeow NFT Tracker V3/cogs/help.py
+++ Kizmeow NFT Tracker V3/cogs/help.py
@@ -1,99 +1,118 @@
-from certifi import contents
 import discord
-from discord.commands import slash_command
 from discord.ext import commands
-from core.cog_core import cogcore
-from discord.ui import Button,View
+from discord.commands import slash_command
+from discord.ui import Button, View
 
-class help(cogcore):
-    @slash_command(name='help',description='display help message')
+
+class help(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash_command(name='help', description='display help message')
     async def help(
-        self,
-        ctx: discord.ApplicationContext
+            self,
+            ctx: discord.ApplicationContext
     ):
-      #----------------------------------------------------------------------------------------------------buttons
-      b_bot_info = Button(label='bot info', style=discord.ButtonStyle.blurple)
-      b_system_commands = Button(label='system commands', style=discord.ButtonStyle.blurple)
-      b_nft_commands = Button(label='NFT commands', style=discord.ButtonStyle.blurple)
-      b_return = Button(style=discord.ButtonStyle.gray,emoji='↩️')
-      #----------------------------------------------------------------------------------------------------buttons
+        # ----------------------------------------------------------------------------------------------------buttons
+        b_bot_info = Button(label='Bot info', style=discord.ButtonStyle.blurple)
+        b_system_commands = Button(label='System commands', style=discord.ButtonStyle.blurple)
+        b_nft_commands = Button(label='NFT commands', style=discord.ButtonStyle.blurple)
+        b_return = Button(style=discord.ButtonStyle.gray, emoji='↩️')
 
-      # callback
-      #----------------------------------------------------------------------------------------------------return callback
-      async def b_return_callback(interaction):
+        # ----------------------------------------------------------------------------------------------------buttons
+
+        # callback
+        # ----------------------------------------------------------------------------------------------------return callback
+        async def b_return_callback(interaction):
+            view = View()
+
+            view.add_item(b_bot_info)
+            view.add_item(b_system_commands)
+            view.add_item(b_nft_commands)
+            view.add_item(b_return)
+
+            embed = discord.Embed(title='**Help**', description='`click the button below to select different section`',
+                                  color=0xFFA46E)
+            embed.add_field(name='**Bot info**', value='`information about the bot`', inline=False)
+            embed.add_field(name='**System commands**', value='`list of system commands`', inline=False)
+            embed.add_field(name='**NFT commands**', value='`list of NFT commands`', inline=False)
+            await interaction.response.edit_message(embed=embed, view=view)
+
+        b_return.callback = b_return_callback
+
+        # ----------------------------------------------------------------------------------------------------return callback
+
+        # ----------------------------------------------------------------------------------------------------bot info callback
+        async def b_bot_info_callback(interaction):
+            embed = discord.Embed(title='**Bot info**', description='', color=0xFFA46E)
+            embed.add_field(name='**Bot name**', value='[Kizmeow NFT Tracker V3#2184]('
+                                                   'https://discord.com/api/oauth2/authorize?client_id'
+                                                   '=923512417907015693&permissions=277025508352&scope=applications'
+                                                   '.commands%20bot)', inline=False)
+            embed.add_field(name='**Developer**', value='[Xeift#1230](https://discord.com/users/874806243208871977)',
+                            inline=False)
+            embed.add_field(name='**Bot avatar illustrator**', value='[Kiyue](https://www.facebook.com/profile.php?id'
+                                                                 '=100026170072950)', inline=False)
+            embed.add_field(name='**Programming language**', value='[Python](https://www.python.org/)', inline=False)
+            b_github = Button(label='GitHub', style=discord.ButtonStyle.link,
+                              url='https://github.com/Xeift/Kizmeow-OpenSea-and-Etherscan-Discord-Bot')
+            view = View()
+            view.add_item(b_github)
+            view.add_item(b_return)
+            await interaction.response.edit_message(embed=embed, view=view)
+
+        b_bot_info.callback = b_bot_info_callback
+
+        # ----------------------------------------------------------------------------------------------------bot info callback
+
+        # ----------------------------------------------------------------------------------------------------system commands callback
+        async def b_system_commands_callback(interaction):
+            embed = discord.Embed(title='**System commands**', description='', color=0xFFA46E)
+            embed.add_field(name='**/help**', value='`display this message`', inline=False)
+            embed.add_field(name='**/meow**', value='`return bot latency`', inline=False)
+            embed.add_field(name='**/invite**', value='`invite Kizmeow to your server`', inline=False)
+
+            view = View()
+            view.add_item(b_return)
+            await interaction.response.edit_message(embed=embed, view=view)
+
+        b_system_commands.callback = b_system_commands_callback
+
+        # ----------------------------------------------------------------------------------------------------system commands callback
+
+        # ----------------------------------------------------------------------------------------------------NFT commands callback
+        async def b_nft_commands_callback(interaction):
+            embed = discord.Embed(title='**NFT commands**', description='', color=0xFFA46E)
+            embed.add_field(name='**/project_realtime**', value='`display project realtime information`', inline=False)
+            embed.add_field(name='**/project_history**', value='`display project history information`', inline=False)
+            embed.add_field(name='**/project_nft**', value='`display information of specific NFT`', inline=False)
+
+            view = View()
+            view.add_item(b_return)
+            await interaction.response.edit_message(embed=embed, view=view)
+
+        b_nft_commands.callback = b_nft_commands_callback
+        # ----------------------------------------------------------------------------------------------------NFT commands callback
+
+        # ----------------------------------------------------------------------------------------------------default buttons
         view = View()
 
         view.add_item(b_bot_info)
         view.add_item(b_system_commands)
         view.add_item(b_nft_commands)
         view.add_item(b_return)
+        # ----------------------------------------------------------------------------------------------------default buttons
 
-        embed=discord.Embed(title='**[help]**', description='click the button below to select different section', color=0xFFA46E)
-        embed.add_field(name='bot info',value='information about the bot',inline=False)
-        embed.add_field(name='system commands',value='list of system commands',inline=False)
-        embed.add_field(name='NFT commands',value='list of NFT commands',inline=False)
-        await interaction.response.edit_message(embed=embed,view=view)
-      b_return.callback = b_return_callback
-      #----------------------------------------------------------------------------------------------------return callback
+        # ----------------------------------------------------------------------------------------------------default embed
+        embed = discord.Embed(title='**Help**', description='`click the button below to select different section`',
+                              color=0xFFA46E)
+        embed.add_field(name='**Dot info**', value='`information about the bot`', inline=False)
+        embed.add_field(name='**System commands**', value='`list of system commands`', inline=False)
+        embed.add_field(name='**NFT commands**', value='`list of NFT commands`', inline=False)
+        await ctx.respond(embed=embed, view=view)
+        # ----------------------------------------------------------------------------------------------------default embed
 
-      #----------------------------------------------------------------------------------------------------bot info callback
-      async def b_bot_info_callback(interaction):
-        embed=discord.Embed(title='**[bot info]**', description='', color=0xFFA46E)
-        embed.add_field(name='bot name',value='Kizmeow NFT Tracker V3#2184',inline=False)
-        embed.add_field(name='developer',value='Xeift#1230',inline=False)
-        embed.add_field(name='bot avatar illustrator',value='Kiyue',inline=False)
-        embed.add_field(name='programming language', value='Python', inline=False)
-        b_github = Button(label='GitHub',style=discord.ButtonStyle.link,url='https://github.com/Xeift/Kizmeow-OpenSea-and-Etherscan-Discord-Bot')
-        view = View()
-        view.add_item(b_github)
-        view.add_item(b_return)
-        await interaction.response.edit_message(embed=embed,view=view)
-      b_bot_info.callback = b_bot_info_callback
-      #----------------------------------------------------------------------------------------------------bot info callback
 
-      #----------------------------------------------------------------------------------------------------system commands callback
-      async def b_system_commands_callback(interaction):
-        embed=discord.Embed(title='**[system commands]**', description='', color=0xFFA46E)
-        embed.add_field(name='`/help`',value='display this message',inline=False)
-        embed.add_field(name='`/meow`',value='return bot latency',inline=False)
-        embed.add_field(name='`/invite`',value='invite Kizmeow to your server',inline=False)
-        # b_github = Button(label='GitHub',style=discord.ButtonStyle.link,url='https://github.com/Xeift/Kizmeow-OpenSea-and-Etherscan-Discord-Bot')
-        view = View()
-        # view.add_item(b_github)
-        view.add_item(b_return)
-        await interaction.response.edit_message(embed=embed,view=view)
-      b_system_commands.callback = b_system_commands_callback
-      #----------------------------------------------------------------------------------------------------system commands callback
-
-      #----------------------------------------------------------------------------------------------------NFT commands callback
-      async def b_nft_commands_callback(interaction):
-        embed=discord.Embed(title='**[NFT commands]**', description='', color=0xFFA46E)
-        embed.add_field(name='`/project_realtime`',value='display project realtime information',inline=False)
-        embed.add_field(name='`/project_history`',value='display project history information',inline=False)
-        embed.add_field(name='`/project_nft`', value='display information of specific NFT', inline=False)
-        # b_github = Button(label='GitHub',style=discord.ButtonStyle.link,url='https://github.com/Xeift/Kizmeow-OpenSea-and-Etherscan-Discord-Bot')
-        view = View()
-        # view.add_item(b_github)
-        view.add_item(b_return)
-        await interaction.response.edit_message(embed=embed,view=view)
-      b_nft_commands.callback = b_nft_commands_callback
-      #----------------------------------------------------------------------------------------------------NFT commands callback      
-
-      #----------------------------------------------------------------------------------------------------default buttons
-      view = View()
-
-      view.add_item(b_bot_info)
-      view.add_item(b_system_commands)
-      view.add_item(b_nft_commands)
-      view.add_item(b_return)
-      #----------------------------------------------------------------------------------------------------default buttons
-
-      #----------------------------------------------------------------------------------------------------default embed
-      embed=discord.Embed(title='**[help]**', description='click the button below to select different section', color=0xFFA46E)
-      embed.add_field(name='bot info',value='information about the bot',inline=False)
-      embed.add_field(name='system commands',value='list of system commands',inline=False)
-      embed.add_field(name='NFT commands',value='list of NFT commands',inline=False)
-      await ctx.respond(embed=embed,view=view)
-      #----------------------------------------------------------------------------------------------------default embed
 def setup(bot):
-  bot.add_cog(help(bot))
+    bot.add_cog(help(bot))
--- Kizmeow NFT Tracker V3/cogs/meow.py
+++ Kizmeow NFT Tracker V3/cogs/meow.py
@@ -1,16 +1,21 @@
 import discord
-from discord.commands import slash_command
 from discord.ext import commands
-from core.cog_core import cogcore
+from discord.commands import slash_command
 
-class meow(cogcore):
-    @slash_command(name='meow',description='return bot latency')
+
+class meow(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash_command(name='meow', description='return bot latency')
     async def meow(
-        self,
-        ctx: discord.ApplicationContext
+            self,
+            ctx: discord.ApplicationContext
     ):
-        embed=discord.Embed(title='Nyaa!', description=f'{self.bot.latency*1000:.2f} ms', color=0xFFA46E)
+        embed = discord.Embed(title='Nyaa!', description=f'{self.bot.latency * 1000:.2f} ms', color=0xFFA46E)
         await ctx.respond(embed=embed)
 
-def setup(bot):# add cog
+
+def setup(bot):  # add cog
     bot.add_cog(meow(bot))
