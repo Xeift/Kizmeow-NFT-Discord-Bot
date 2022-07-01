a8c50f2985482058037f180dbfcb25550fc651b8
DOFER998 <67563470+DOFER998@users.noreply.github.com>
2022-07-01 16:23:31 +0300

    Activity type

--- Kizmeow NFT Tracker V3/main.py
+++ Kizmeow NFT Tracker V3/main.py
@@ -1,4 +1,7 @@
 import discord
+import asyncio
+import json
+import urllib.request as ur
 
 bot = discord.Bot(intents=discord.Intents.all(), )
 
@@ -6,6 +9,22 @@ bot = discord.Bot(intents=discord.Intents.all(), )
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+    while True:
+        url = 'https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=' +etherscan_api_key  # api url
+
+        site = ur.urlopen(url)
+        page = site.read()
+        contents = page.decode()
+        data = json.loads(contents)
+
+        SafeGasPrice = data['result']['SafeGasPrice']
+        ProposeGasPrice = data['result']['ProposeGasPrice']
+        FastGasPrice = data['result']['FastGasPrice']
+
+        presence_ctx = '⚡️' + FastGasPrice + '| 🚶🏼‍♂️' + ProposeGasPrice + '| 🐢' + SafeGasPrice
+
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=presence_ctx))
+        await asyncio.sleep(25)
 
 
 extensions = [  # load cogs
@@ -15,6 +34,10 @@ extensions = [  # load cogs
     'cogs.help',
     # --------------------system commands
 
+    # --------------------etherscan commands
+    'cogs.gas',
+    'cogs.eth',
+    # --------------------etherscan commands
     # --------------------NFT commands
     'cogs.project_rarity',
     'cogs.project_realtime',
@@ -27,4 +50,4 @@ if __name__ == '__main__':  # import cogs from cogs folder
     for extension in extensions:
         bot.load_extension(extension)
 
-bot.run('BOT_TOKEN')  # bot token
+bot.run('TOKEN')  # bot token
