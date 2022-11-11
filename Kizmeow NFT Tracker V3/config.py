e028594784e9e697dc54155dfd8018fa47f71d0f
c0mrade <killmy666@gmail.com>
2022-11-11 18:32:24 +0300

    Remove

--- Kizmeow NFT Tracker V3/config.py
+++
@@ -1,9 +0,0 @@
-import json
-
-config = {}
-config['ETHERSCAN_API_KEY'] = str(input('enter etherscan api key: '))
-config['DISCORD_BOT_TOKEN'] = str(input('enter discord bot token: '))
-
-print(f'\nsuccess!\nyour config is:\n{config}')
-with open('Kizmeow NFT Tracker V3/config.json','w') as of:
-    json.dump(config,of)
