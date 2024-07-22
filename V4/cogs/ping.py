873759e61591023a15822809cb134c91249ad552
Xeift <xeift.c@gmail.com>
2024-07-22 11:10:33 +0800

    update ping command

--- V4/cogs/ping.py
+++ V4/cogs/ping.py
@@ -4,18 +4,22 @@ from discord.commands import slash_command
 
 
 class ping(commands.Cog):
-
     def __init__(self, bot):
         self.bot = bot
 
     @slash_command(name='ping', description='Return bot latency')
     async def ping(
-            self,
-            ctx: discord.ApplicationContext
+        self,
+        ctx: discord.ApplicationContext
     ):
-        embed = discord.Embed(title='Nyaa! ' f'{self.bot.latency * 1000:.2f} ms', color=0xFFA46E)
+        latency = self.bot.latency
+        embed = discord.Embed(
+            title='Bot latency',
+            description=f'{latency * 1000:.2f} ms ({latency:.3f} s)',
+            color=0xFFA46E
+        )
         await ctx.respond(embed=embed, ephemeral=True)
 
 
-def setup(bot):  # add cog
+def setup(bot):
     bot.add_cog(ping(bot))
--- V4/main.py
+++ V4/main.py
@@ -2,16 +2,14 @@ import os
 import discord
 from dotenv import load_dotenv
 
+
 intents = discord.Intents.default()
 # intents.members=True
 bot = discord.Bot(intents=intents)
 
-
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
-
-
 extensions = [
     'cogs.ping',
 
@@ -20,7 +18,6 @@ extensions = [
 if __name__ == '__main__':  # import cogs from cogs folder
     for extension in extensions:
         bot.load_extension(extension)
-
 
 load_dotenv()
 bot.run(os.getenv('TOKEN'))
