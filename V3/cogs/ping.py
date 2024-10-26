306ba720f96d50c949b9da6ab8c010b271d39ac5
Xeift <xeift.c@gmail.com>
2024-10-26 11:51:57 +0800

    move old code to V3 folder

--- V3/cogs/ping.py
+++ V3/cogs/ping.py
@@ -4,22 +4,18 @@ from discord.commands import slash_command
 
 
 class ping(commands.Cog):
+
     def __init__(self, bot):
         self.bot = bot
 
     @slash_command(name='ping', description='Return bot latency')
     async def ping(
-        self,
-        ctx: discord.ApplicationContext
+            self,
+            ctx: discord.ApplicationContext
     ):
-        latency = self.bot.latency
-        embed = discord.Embed(
-            title='Bot latency',
-            description=f'{latency * 1000:.2f} ms ({latency:.3f} s)',
-            color=0xFFA46E
-        )
+        embed = discord.Embed(title='Nyaa! ' f'{self.bot.latency * 1000:.2f} ms', color=0xFFA46E)
         await ctx.respond(embed=embed, ephemeral=True)
 
 
-def setup(bot):
+def setup(bot):  # add cog
     bot.add_cog(ping(bot))
--- main.py
+++ main.py
@@ -1,56 +1,22 @@
 import os
 import discord
-import asyncio
-from discord.ext import tasks
 from dotenv import load_dotenv
 
-intents=discord.Intents.default()
-intents.members=True
+
+intents = discord.Intents.default()
+# intents.members=True
 bot = discord.Bot(intents=intents)
-
-@tasks.loop(seconds=30)
-async def change_status():
-    servers = str(len(bot.guilds))
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f'{servers} servers\u2764'))
-    await asyncio.sleep(20)
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='/help'))
-
-
-
-@change_status.before_loop
-async def before():
-    await bot.wait_until_ready()
-
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
-    change_status.start()
-
-
-extensions = [  # load cogs
-    # --------------------system commands
-    'cogs.ping',
-    'cogs.help',
-    'cogs.feedback',
-    'cogs.reload_cmds',  # only for debug
-    # --------------------system commands
-
-    # --------------------Web3 commands
-    'cogs.gas',
-    'cogs.eth',
-    # --------------------Web3 commands
-
-    # --------------------NFT commands
-    'cogs.collection',
-    'cogs.nft'
-    # --------------------NFT commands
-]
 
 if __name__ == '__main__':  # import cogs from cogs folder
-    for extension in extensions:
-        bot.load_extension(extension)
+    extensions = [
+        'cogs.ping',
+
+    ]
+    for extension in extensions: bot.load_extension(extension)
 
 load_dotenv()
-bot.run(os.getenv('TOKEN'))  # bot token
-
+bot.run(os.getenv('TOKEN'))
