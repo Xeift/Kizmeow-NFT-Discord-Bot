e529d3e487321b0113b350e3baf68cbc6a1ca168
Xeift <xeift.c@gmail.com>
2024-10-29 14:52:00 +0800

    add format check for get_os_account.py

--- api/ens_to_address.py
+++ api/ens_to_address.py
@@ -10,14 +10,8 @@ WEB3_PROVIDER_URI = os.getenv('WEB3_PROVIDER_URI')
 def ens_to_address(ens):
     w3 = Web3(Web3.HTTPProvider(WEB3_PROVIDER_URI))
     address = w3.ens.address(ens)
-    print(address)
 
     if address:
         return address
     else:
         return False
-
-
-# res = ens_to_address('vitalik.eth')
-res = ens_to_address('vitaliktertert356666666666666666.eth')
-print(res)
---
+++ api/get_os_account.py
@@ -0,0 +1,52 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+from ens_to_address import ens_to_address
+
+load_dotenv()
+OPENSEA_API_KEY = os.getenv('OPENSEA_API_KEY')
+
+
+def get_os_account(account_name_or_address):
+    if account_name_or_address.endswith('.eth'):
+        account_name_or_address = ens_to_address(account_name_or_address)
+        if account_name_or_address == False:
+            return (False, 'This ENS does not exsist. Please check your input.')
+
+    url = f'https://api.opensea.io/api/v2/accounts/{account_name_or_address}'
+    headers = {
+        'accept': 'application/json',
+        'x-api-key': OPENSEA_API_KEY
+    }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        res = response.json()
+        print(res)
+        return (True, 'Some valid result')
+    elif response.status_code == 400:
+        return (False, 'The account does not exist or can not be parsed.')
+    else:
+        return (False, 'OpenSea API is currently down. Please try again later.')
+
+
+# --------------------     TEST        --------------------
+
+# valid address
+print(get_os_account('0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045'))
+
+# # invalid address
+# print(get_os_account('0xf8dA6BF26964aF9D7eEd9e03E53415D37aA96045'))
+
+# # valid os username
+# print(get_os_account('RTFKTCLONEXTM'))
+
+# # invalid os username
+# print(get_os_account('dasfre65_fsdh3r_64fsdfs'))
+
+# # valid ens
+# print(get_os_account('vitalik.eth'))
+
+# # invalid ens
+# print(get_os_account('vitaliktertert356666666666666666.eth'))
