9718711ee757aa60cccc25409ffb2b4d5ea4c07b
Xeift <xeift.c@gmail.com>
2025-01-14 13:00:43 +0800

    update logic in load_config_from_json()

--- utils/load_config.py
+++ utils/load_config.py
@@ -4,6 +4,17 @@ import json
 def load_config_from_json(uid):
     with open('setting.json', 'r', encoding='utf-8') as file:
         setting_data = json.load(file)
-        setting_data = setting_data[uid]
+        setting_data = setting_data.setdefault(
+            uid,
+            {
+                'button': False,
+                'language': 'en-US',
+                'visibility': False
+            }
+        )
 
-    return (setting_data['button'], setting_data['language'], setting_data['visibility'])
+    return (
+        setting_data['button'],
+        setting_data['language'],
+        setting_data['visibility']
+    )
