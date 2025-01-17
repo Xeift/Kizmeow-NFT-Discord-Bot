4465a1bbc5e6ef04e91957922a4ee91813c7a1e6
Xeift <xeift.c@gmail.com>
2025-01-17 08:34:55 +0800

    add valid info test case

--- api/get_os_nft.py
+++ api/get_os_nft.py
@@ -7,8 +7,8 @@ load_dotenv()
 OPENSEA_API_KEY = os.getenv('OPENSEA_API_KEY')
 
 
-def get_os_collection(collection):
-    url = f'https://api.opensea.io/api/v2/collections/{collection}'
+def get_os_nft(address, chain, token_id):
+    url = f'https://api.opensea.io/api/v2/chain/{chain}/contract/{address}/nfts/{token_id}'
     headers = {
         'accept': 'application/json',
         'x-api-key': OPENSEA_API_KEY
@@ -19,6 +19,7 @@ def get_os_collection(collection):
         res = response.json()
         return (True, res)
     elif response.status_code == 400:
+        print(response.text)
         return (False, 'The collection does not exist or can not be parsed.')
     else:
         return (False, 'OpenSea API is currently down. Please try again later.')
@@ -26,8 +27,12 @@ def get_os_collection(collection):
 
 # --------------------     TEST        --------------------
 
-# # valid collection slug
-# print(get_os_collection('azuki'))
+# valid info
+print(get_os_nft(
+    '0xbd3531da5cf5857e7cfaa92426877b022e612cf8',
+    'ethereum',
+    '5712'
+))
 
 # # invalid collection slug
-# print(get_os_collection('azukite'))
+# print(get_os_nft('azukite'))
