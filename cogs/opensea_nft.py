f643c7d5a62a128484174479bbeb1e40727aff26
Xeift <xeift.c@gmail.com>
2025-01-18 09:55:33 +0800

    add makeNftEmbed()

--- cogs/opensea_nft.py
+++ cogs/opensea_nft.py
@@ -39,110 +39,122 @@ class opensea_nft(commands.Cog):
             'Select the collection. [Quick Select]',
             autocomplete=basic_autocomplete(collection_name_autocomplete)
         ),
-        chain: Option(
-            str,
-            'The chain of the NFT',
-            required=False
-        ),
-        address: Option(
-            str,
-            'The address of the NFT',
-            required=False
-        ),
+        # chain: Option(
+        #     str,
+        #     'The chain of the NFT',
+        #     required=False
+        # ),
+        # address: Option(
+        #     str,
+        #     'The address of the NFT',
+        #     required=False
+        # ),
     ):
         await ctx.defer()
-        disable_link_button = not enable_link_button
-        (success, nft_data) = get_os_nft(chain, address, token_id)
-        embed = Embed(color=0xFFA46E)
-        view = View()
 
-        if success:
-            address = account_data['address']
-            username = account_data['username']
-            bio = account_data['bio']
-            joined_date = account_data['joined_date']
-            short_address = account_data['address'][:7]
-            pfp_img = account_data['profile_image_url']
-            banner_img = account_data['banner_image_url']
-            opensea_url = f'https://opensea.io/{address}'
-            etherscan_url = f'https://etherscan.io/address/{address}'
-            website_url = account_data['website']
-            social_media_accounts = account_data['social_media_accounts']
+        with open('collection_name_autocomplete.json', 'r') as of:
+            collection_name_data = json.load(of)
+            
+        if quick_select == '[Manually enter contract address]':
+            print('edit and add select menu + input modal')
+        elif collection in collection_name_data:
+            collection = collection_name_data[collection]['slug']
+            print('call func to get embed')
+        else:
+            print('collection not found')
+            
+        def makeNftEmbed(chain, address, token_id):
+            (success, nft_data) = get_os_nft(chain, address, token_id)
+            embed = Embed(color=0xFFA46E)
+            view = View()
 
-            embed.title = f'OpenSea Account Info of {short_address}'
-            embed.set_thumbnail(url=pfp_img)
-            if banner_img != '':
-                embed.set_image(url=banner_img)
-            embed.set_footer(
-                text='Source: OpenSea API',
-                icon_url='https://raw.githubusercontent.com/Xeift/Kizmeow-NFT-Discord-Bot/refs/heads/main/img/opensea_logo.png'
-            )
+            if success:
+                address = account_data['address']
+                username = account_data['username']
+                bio = account_data['bio']
+                joined_date = account_data['joined_date']
+                short_address = account_data['address'][:7]
+                pfp_img = account_data['profile_image_url']
+                banner_img = account_data['banner_image_url']
+                opensea_url = f'https://opensea.io/{address}'
+                etherscan_url = f'https://etherscan.io/address/{address}'
+                website_url = account_data['website']
+                social_media_accounts = account_data['social_media_accounts']
 
-            embed.add_field(
-                name='Address', value=f'[{address}]({etherscan_url})', inline=False)
-            embed.add_field(name='Username', value=username, inline=True)
-            embed.add_field(name='Bio', value=bio, inline=True)
-            embed.add_field(name='Joined Date', value=joined_date, inline=True)
-            embed.add_field(
-                name='', value='Note: buttons are disabled by default. If you wish to enable the buttons, set `enable_link_button` parameter to `False`.', inline=False)
-            embed.set_footer(
-                text='Source: OpenSea API',
-                icon_url='https://raw.githubusercontent.com/Xeift/Kizmeow-NFT-Discord-Bot/refs/heads/main/img/opensea_logo.png'
-            )
-            opensea_button = Button(
-                label='OpenSea',
-                style=ButtonStyle.link,
-                url=opensea_url,
-                emoji=PartialEmoji(name='opensea_icon_transparent',
-                                   id=1300799256730538047),
-                disabled=disable_link_button
-            )
-            view.add_item(opensea_button)
+                embed.title = f'OpenSea Account Info of {short_address}'
+                embed.set_thumbnail(url=pfp_img)
+                if banner_img != '':
+                    embed.set_image(url=banner_img)
+                embed.set_footer(
+                    text='Source: OpenSea API',
+                    icon_url='https://raw.githubusercontent.com/Xeift/Kizmeow-NFT-Discord-Bot/refs/heads/main/img/opensea_logo.png'
+                )
 
-            etherscan_button = Button(
-                label='Etherscan',
-                style=ButtonStyle.link,
-                url=etherscan_url,
-                emoji=PartialEmoji(name='etherscan_icon_transparent',
-                                   id=1300797214720917596),
-                disabled=disable_link_button
-            )
-            view.add_item(etherscan_button)
+                embed.add_field(
+                    name='Address', value=f'[{address}]({etherscan_url})', inline=False)
+                embed.add_field(name='Username', value=username, inline=True)
+                embed.add_field(name='Bio', value=bio, inline=True)
+                embed.add_field(name='Joined Date', value=joined_date, inline=True)
+                embed.add_field(
+                    name='', value='Note: buttons are disabled by default. If you wish to enable the buttons, set `enable_link_button` parameter to `False`.', inline=False)
+                embed.set_footer(
+                    text='Source: OpenSea API',
+                    icon_url='https://raw.githubusercontent.com/Xeift/Kizmeow-NFT-Discord-Bot/refs/heads/main/img/opensea_logo.png'
+                )
+                opensea_button = Button(
+                    label='OpenSea',
+                    style=ButtonStyle.link,
+                    url=opensea_url,
+                    emoji=PartialEmoji(name='opensea_icon_transparent',
+                                       id=1300799256730538047),
+                    disabled=disable_link_button
+                )
+                view.add_item(opensea_button)
 
-            website_button = Button(
-                label='Website',
-                style=ButtonStyle.link,
-                url=website_url,
-                emoji='🔗',
-                disabled=disable_link_button
-            )
-            if website_url != '':
-                view.add_item(website_button)
+                etherscan_button = Button(
+                    label='Etherscan',
+                    style=ButtonStyle.link,
+                    url=etherscan_url,
+                    emoji=PartialEmoji(name='etherscan_icon_transparent',
+                                       id=1300797214720917596),
+                    disabled=disable_link_button
+                )
+                view.add_item(etherscan_button)
 
-            for social_media_account in social_media_accounts:
-                platform = social_media_account['platform']
-                username = social_media_account['username']
+                website_button = Button(
+                    label='Website',
+                    style=ButtonStyle.link,
+                    url=website_url,
+                    emoji='🔗',
+                    disabled=disable_link_button
+                )
+                if website_url != '':
+                    view.add_item(website_button)
 
-                if platform == 'twitter':
-                    x_button = Button(
-                        label='X',
-                        style=ButtonStyle.link,
-                        url=f'https://x.com/{username}',
-                        emoji=PartialEmoji(name='x_icon_transparent',
-                                           id=1300788826905772063),
-                        disabled=disable_link_button
-                    )
-                    view.add_item(x_button)
-                elif platform == 'instagram':
-                    instagram_button = Button(
-                        label='Instagram',
-                        style=ButtonStyle.link,
-                        url=f'https://www.instagram.com/{username}',
-                        emoji=PartialEmoji(
-                            name='instagram_icon_transparent', id=1300788845842927708),
-                        disabled=disable_link_button
-                    )
-                    view.add_item(instagram_button)
+                for social_media_account in social_media_accounts:
+                    platform = social_media_account['platform']
+                    username = social_media_account['username']
+
+                    if platform == 'twitter':
+                        x_button = Button(
+                            label='X',
+                            style=ButtonStyle.link,
+                            url=f'https://x.com/{username}',
+                            emoji=PartialEmoji(name='x_icon_transparent',
+                                               id=1300788826905772063),
+                            disabled=disable_link_button
+                        )
+                        view.add_item(x_button)
+                    elif platform == 'instagram':
+                        instagram_button = Button(
+                            label='Instagram',
+                            style=ButtonStyle.link,
+                            url=f'https://www.instagram.com/{username}',
+                            emoji=PartialEmoji(
+                                name='instagram_icon_transparent', id=1300788845842927708),
+                            disabled=disable_link_button
+                        )
+                        view.add_item(instagram_button)
 
         else:
             embed.title = '[Failed]'
