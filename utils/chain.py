2b641fe4e0a1567901add9d7666644169a207271
Xeift <xeift.c@gmail.com>
2025-01-23 08:55:54 +0800

    add get_info_by_code()

--- utils/chain.py
+++ utils/chain.py
@@ -6,6 +6,15 @@ with open('chain_detail.json', 'r', encoding='utf-8') as file:
 def get_name_by_code(chain_code):
     return chain_data[chain_code]['chain_name']
 
+def get_info_by_code(chain_code):
+    single_chain_data = chain_data[chain_code]
+    return (
+        single_chain_data['chain_name'],
+        single_chain_data['exp_name'],
+        single_chain_data['exp_url'],
+        single_chain_data['exp_emoji'],        
+        single_chain_data['ticker']
+    )
 
 # --------------------     TEST        --------------------
 # print(get_name_by_code('ethereum'))
