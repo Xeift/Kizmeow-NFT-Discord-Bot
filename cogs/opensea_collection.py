3d98cdcab0067c1b9b9cc6311b73ec1089c21a15
Xeift <xeift.c@gmail.com>
2025-02-10 11:19:56 +0800

    minor fixes

--- cogs/opensea_collection.py
+++ cogs/opensea_collection.py
@@ -115,7 +115,10 @@ class opensea_collection(commands.Cog):
                 label='OpenSea',
                 style=ButtonStyle.link,
                 url=opensea_url,
-                emoji=PartialEmoji(name='opensea_icon',id=1326452492644515963),
+                emoji=PartialEmoji(
+                    name='opensea_icon',
+                    id=1326452492644515963
+                ),
                 disabled=disable_link_button
             )
             if opensea_url != '':
