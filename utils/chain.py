11a4bf9b4728971063606e94e7f9a822959ae5f2
Xeift <xeift.c@gmail.com>
2025-02-16 11:00:14 +0800

    add get_available_chains()

--- utils/chain.py
+++ utils/chain.py
@@ -3,30 +3,26 @@ import json
 with open('chain_detail.json', 'r', encoding='utf-8') as file:
     chain_data = json.load(file)
 
-
 def get_name_by_code(chain_code):
     return chain_data[chain_code]['chain_name']
-
 
 def get_info_by_code(chain_code):
     single_chain_data = chain_data[chain_code]
     return (
         single_chain_data['chain_name'],
         single_chain_data['exp_name'],
-        single_chain_data['exp_address_url'],
-        single_chain_data['exp_token_url'],
-        single_chain_data['exp_emoji'],
-        single_chain_data['ticker'],
-        single_chain_data['token_standards'],
+        single_chain_data['exp_url'],
+        single_chain_data['exp_emoji'],        
+        single_chain_data['ticker']
     )
 
-
 def get_code_by_name(chain_name):
-    with open('chain_detail.json', 'r') as file:
-        data = json.load(file)
-    for k, v in data.items():
+    for k, v in chain_data.items():
         if v['chain_name'] == chain_name:
             return k
+
+def get_available_chains():
+    return [v['chain_name'] for v in chain_data.values()]
 
 # --------------------     TEST        --------------------
 # print(get_name_by_code('ethereum'))
