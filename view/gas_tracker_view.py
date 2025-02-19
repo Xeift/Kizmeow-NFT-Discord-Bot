14181b614d173ae45ce619e98b962b55f3b68969
Xeift <xeift.c@gmail.com>
2025-02-19 22:43:52 +0800

    add gas_blocknative_embed

--- view/gas_tracker_view.py
+++ view/gas_tracker_view.py
@@ -8,10 +8,24 @@ def gas_etherscan_view(view):
         style=ButtonStyle.link,
         url='https://etherscan.io/gastracker',
         emoji=PartialEmoji(
-            name='etherscan_icon_transparent',
+            name='etherscan_icon',
             id=1326452528920920125
         )
     )
     view.add_item(etherscan_button)
+
+    return view
+
+def gas_blocknative_view(view):
+    blocknative_button = Button(
+        label='Blocknative Gas Tracker',
+        style=ButtonStyle.link,
+        url='https://www.blocknative.com/gas-estimator',
+        emoji=PartialEmoji(
+            name='blocknative_icon',
+            id=1341781218827698247
+        )
+    )
+    view.add_item(blocknative_button)
 
     return view
