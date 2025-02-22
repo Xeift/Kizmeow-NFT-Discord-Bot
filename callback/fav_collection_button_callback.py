5c257616e0948c3742ee807a6338a35517abb3c3
Xeift <xeift.c@gmail.com>
2025-02-22 15:49:05 +0800

    move fav_collection_button_callback

---
+++ callback/fav_collection_button_callback.py
@@ -0,0 +1,41 @@
+from discord import (ApplicationContext, ButtonStyle, Embed, IntegrationType,
+                     InteractionContextType, Option, PartialEmoji)
+
+from embed.err_embed import general_err_embed
+from utils.load_config import load_config_from_json, update_config_to_json
+
+
+async def fav_collection_button_callback(
+        interaction,
+        author_id,
+        favorite_collections,
+        collection_name,
+        collection
+    ):
+    if author_id != interaction.user.id:
+        return
+
+    embed = Embed(color=0xFFA46E)
+    if interaction.data['custom_id'] == '❤️':
+        if len(favorite_collections) >= 20:
+            embed = general_err_embed(
+                'You can only have 20 favorite collection slots.')
+        else:
+            favorite_collections[collection_name] = {
+                'slug': collection
+            }
+            update_config_to_json(
+                uid=str(interaction.user.id),
+                favorite_collections=favorite_collections
+            )
+            embed.title = 'Collection added'
+            embed.description = f'The collection `{collection_name}` has added to your favorite collections.'
+    else:
+        del favorite_collections[collection_name]
+        update_config_to_json(
+            uid=str(interaction.user.id),
+            favorite_collections=favorite_collections
+        )
+        embed.title = 'Collection removed'
+        embed.description = f'The collection `{collection_name}` has removed from your favorite collections.'
+    await interaction.response.send_message(embed=embed)
--- cogs/opensea_collection.py
+++ cogs/opensea_collection.py
@@ -9,6 +9,8 @@ from discord.utils import basic_autocomplete
 
 from api.get_os_collection import get_os_collection
 from api.get_os_collection_statistics import get_os_collection_statistics
+from callback.fav_collection_button_callback import \
+    fav_collection_button_callback
 from embed.err_embed import general_err_embed
 from utils.chain import get_info_by_code
 from utils.load_config import load_config_from_json, update_config_to_json
@@ -284,36 +286,13 @@ class opensea_collection(commands.Cog):
                         '❤️'
                     )
 
-                async def fav_collection_button_callback(interaction):
-                    if ctx.author != interaction.user:
-                        return
-
-                    embed = Embed(color=0xFFA46E)
-                    if interaction.data['custom_id'] == '❤️':
-                        if len(favorite_collections) >= 20:
-                            embed = general_err_embed(
-                                'You can only have 20 favorite collection slots.')
-                        else:
-                            favorite_collections[collection_name] = {
-                                'slug': collection
-                            }
-                            update_config_to_json(
-                                uid=str(interaction.user.id),
-                                favorite_collections=favorite_collections
-                            )
-                            embed.title = 'Collection added'
-                            embed.description = f'The collection `{collection_name}` has added to your favorite collections.'
-                    else:
-                        del favorite_collections[collection_name]
-                        update_config_to_json(
-                            uid=str(interaction.user.id),
-                            favorite_collections=favorite_collections
-                        )
-                        embed.title = 'Collection removed'
-                        embed.description = f'The collection `{collection_name}` has removed from your favorite collections.'
-                    await interaction.response.send_message(embed=embed, ephemeral=not visibility)
-
-                fav_collection_button.callback = fav_collection_button_callback
+                fav_collection_button.callback = lambda interaction: fav_collection_button_callback(
+                    interaction,
+                    ctx.author.id,
+                    favorite_collections,
+                    collection_name,
+                    collection
+                )
                 view.add_item(fav_collection_button)
 
         else:
