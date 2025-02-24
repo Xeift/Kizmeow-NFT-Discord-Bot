93e2c2a93990bdf2e8ac9ace90f664c756d524f0
Xeift <xeift.c@gmail.com>
2025-02-24 12:14:24 +0800

    move embed to address_converter_embed

--- cogs/address_converter.py
+++ cogs/address_converter.py
@@ -9,7 +9,7 @@ from embed.opensea_account_embed import opensea_account_embed
 from utils.load_config import load_config_from_json
 from view.button import (etherscan_button, instagram_button, opensea_button,
                          website_button, x_button)
-from web3 import Web3
+from embed.address_converter_embed import address_converter_embed
 
 class address_converter(commands.Cog):
     def __init__(self, bot):
@@ -46,25 +46,8 @@ class address_converter(commands.Cog):
             _,
             _
         ) = load_config_from_json(mid)
-        embed = Embed(
-            color=0xFFA46E,
-            title='Address converter'
-        )
-        embed.add_field(
-            name='Checksum address',
-            value=f'```{Web3.to_checksum_address(address)}```'
-        )
-        embed.add_field(
-            name='Lower case address',
-            value=f'```{address.lower()}```'
-        )
-        embed.add_field(
-            name='Upper case address',
-            value=f'```{address.upper()}```'
-        )
 
-
-        await ctx.respond(embed=embed)
+        await ctx.respond(embed=address_converter_embed(address))
 
         # if success:
             
