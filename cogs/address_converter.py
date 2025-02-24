6bbb6a625b9ab9206bce93e597c9ad9c9b9c45a3
Xeift <xeift.c@gmail.com>
2025-02-24 08:06:27 +0800

    fix embed

--- cogs/address_converter.py
+++ cogs/address_converter.py
@@ -46,8 +46,24 @@ class address_converter(commands.Cog):
             _,
             _
         ) = load_config_from_json(mid)
-        embed = Embed(title='Address converter')
-        embed.add_field(name='Checksum address', value=Web3.to_checksum_address(address))
+        embed = Embed(
+            color=0xFFA46E,
+            title='Address converter'
+        )
+        embed.add_field(
+            name='Checksum address',
+            value=f'```{Web3.to_checksum_address(address)}```'
+        )
+        embed.add_field(
+            name='Lower case address',
+            value=f'```{address.lower()}```'
+        )
+        embed.add_field(
+            name='Upper case address',
+            value=f'```{address.upper()}```'
+        )
+
+
         await ctx.respond(embed=embed)
 
         # if success:
